1f42095dcdf11e1081b5cde8aa7c258cb9236704
quanchaohao <aa1080711@163.com>
2022-10-30 22:34:54 +0800

    wacig session 07

--- code/code.go
+++ code/code.go
@@ -81,6 +81,14 @@ const (
 	OpArray
 	OpHash
 	OpIndex
+
+	OpCall
+
+	OpReturnValue
+	OpReturn
+
+	OpGetLocal
+	OpSetLocal
 )
 
 type Definition struct {
@@ -120,6 +128,14 @@ var definitions = map[Opcode]*Definition{
 	OpArray: {"OpArray", []int{2}},
 	OpHash:  {"OpHash", []int{2}},
 	OpIndex: {"OpIndex", []int{}},
+
+	OpCall: {"OpCall", []int{1}},
+
+	OpReturnValue: {"OpReturnValue", []int{}},
+	OpReturn:      {"OpReturn", []int{}},
+
+	OpGetLocal: {"OpGetLocal", []int{1}},
+	OpSetLocal: {"OpSetLocal", []int{1}},
 }
 
 func Lookup(op byte) (*Definition, error) {
@@ -151,6 +167,8 @@ func Make(op Opcode, operands ...int) []byte {
 		switch width {
 		case 2:
 			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
+		case 1:
+			instruction[offset] = byte(o)
 		}
 		offset += width
 	}
@@ -166,6 +184,8 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 		switch width {
 		case 2:
 			operands[i] = int(ReadUint16(ins[offset:]))
+		case 1:
+			operands[i] = int(ReadUint8(ins[offset:]))
 		}
 
 		offset += width
@@ -173,6 +193,8 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 
 	return operands, offset
 }
+
+func ReadUint8(ins Instructions) uint8 { return uint8(ins[0]) }
 
 func ReadUint16(ins Instructions) uint16 {
 	return binary.BigEndian.Uint16(ins)
