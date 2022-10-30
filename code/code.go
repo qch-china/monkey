72fada8e5e69a035616b28f3402ddb87b27395bb
quanchaohao <aa1080711@163.com>
2022-10-30 22:31:54 +0800

    wacig session 05

--- code/code.go
+++ code/code.go
@@ -74,6 +74,9 @@ const (
 	OpJump
 
 	OpNull
+
+	OpGetGlobal
+	OpSetGlobal
 )
 
 type Definition struct {
@@ -106,6 +109,9 @@ var definitions = map[Opcode]*Definition{
 	OpJump:          {"OpJump", []int{2}},
 
 	OpNull: {"OpNull", []int{}},
+
+	OpGetGlobal: {"OpGetGlobal", []int{2}},
+	OpSetGlobal: {"OpSetGlobal", []int{2}},
 }
 
 func Lookup(op byte) (*Definition, error) {
