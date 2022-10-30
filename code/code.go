5cfec1b427652b4c961d33e2543141d3acd70666
quanchaohao <aa1080711@163.com>
2022-10-30 22:28:19 +0800

    wacig session 03

--- code/code.go
+++ code/code.go
@@ -51,7 +51,24 @@ type Opcode byte
 
 const (
 	OpConstant Opcode = iota
+
 	OpAdd
+
+	OpPop
+
+	OpSub
+	OpMul
+	OpDiv
+
+	OpTrue
+	OpFalse
+
+	OpEqual
+	OpNotEqual
+	OpGreaterThan
+
+	OpMinus
+	OpBang
 )
 
 type Definition struct {
@@ -61,7 +78,24 @@ type Definition struct {
 
 var definitions = map[Opcode]*Definition{
 	OpConstant: {"OpConstant", []int{2}},
-	OpAdd:      {"OpAdd", []int{}},
+
+	OpAdd: {"OpAdd", []int{}},
+
+	OpPop: {"OpPop", []int{}},
+
+	OpSub: {"OpSub", []int{}},
+	OpMul: {"OpMul", []int{}},
+	OpDiv: {"OpDiv", []int{}},
+
+	OpTrue:  {"OpTrue", []int{}},
+	OpFalse: {"OpFalse", []int{}},
+
+	OpEqual:       {"OpEqual", []int{}},
+	OpNotEqual:    {"OpNotEqual", []int{}},
+	OpGreaterThan: {"OpGreaterThan", []int{}},
+
+	OpMinus: {"OpMinus", []int{}},
+	OpBang:  {"OpBang", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
